4bca097c48ce31b1001a863c45ff4fb603b946ab
Stoyan Nikolov <59065882+stoine92@users.noreply.github.com>
2022-07-12 15:29:19 +0100

    Update on the contactForm

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+export const AppointmentsPage = ({contacts, appointments, addAppointments}) => {
+
+
+  const [title, setTitle] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+
+  const test = (e) =>{
+    console.log(contacts);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        
       </section>
       <hr />
       <section>
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { ContactForm } from "../../components/contactForm/ContactForm";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({contacts, addContacts}) => {
   /*
   Define state variables for 
   contact info and duplicate check
-  */
+ */  
+
+ const [name, setName] = useState("");
+ const [phone, setPhone] = useState("");
+ const [email, setEmail] = useState("");
+ const [isAdded, setIsAdded] = useState(false);
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,6 +20,13 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if(!isAdded) {
+      addContacts(name, phone, email);
+      setName("");
+      setPhone("");
+      setEmail("");
+    }
+   
   };
 
   /*
@@ -23,7 +38,13 @@ export const ContactsPage = () => {
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm 
+        name={name}
+        phone={phone}
+        email={email}
+      />
       </section>
+      
       <hr />
       <section>
         <h2>Contacts</h2>
