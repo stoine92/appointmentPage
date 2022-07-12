5074aae5957a120374221bed050c7bab14fc56b7
Stoyan Nikolov <59065882+stoine92@users.noreply.github.com>
2022-07-12 19:51:20 +0100

    Added Regex to the input field

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -13,11 +13,31 @@ export const ContactForm = ({
   return (
     <>
     <form onSubmit={handleSubmit}>
-    <input type="text"></input>
-    <input type="tel"></input>
-    <input type="email"></input>
+    <input type="text"
+    value={name}
+    onChange={(e) => setName(e.target.value)}
+    placeholder="Name"
+    required
+    ></input>
 
+    <input 
+    type="tel"
+    value={phone}
+    onChange={(e) => setPhone(e.target.value)}
+    pattern="((\+44(\s\(0\)\s|\s0\s|\s)?)|0)7\d{3}(\s)?\d{6}"
+    placeholder="Telephone number"
+    required
+    ></input>
 
+    <input 
+    type="email"
+    value={email}
+    onChange={(e) => setEmail(e.target.value)}
+    placeholder='Email address'
+    required
+    ></input>
+
+    <button>Submit</button>
     </form>
     {ContactForm}
     </>
